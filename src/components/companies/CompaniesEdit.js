fd63a51a684fe8565a0fa56ab89c6db0e43d1907
seb yeates <yeatessa@gmail.com>
2019-03-05 15:26:13 +0000

    Keys added

--- src/components/companies/CompaniesEdit.js
+++ src/components/companies/CompaniesEdit.js
@@ -14,6 +14,7 @@ class CompaniesEdit extends React.Component {
       errors: ''
     }
 
+  
     this.handleChange = this.handleChange.bind(this)
     this.suggestionSelect = this.suggestionSelect.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
